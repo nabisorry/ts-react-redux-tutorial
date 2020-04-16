1cca7d18467acb362053da442e9d9b07e7319a32
nabisorry <cdb131450@gmail.com>
2020-04-16 10:03:00 +0900

    [typescript-redux-01] 타입스크립트 redux로 counter 구현하기

---
+++ src/modules/counter.ts
@@ -0,0 +1,44 @@
+import { ReactType } from "react";
+
+const INCREASE = "counter/INCREASE" as const; //as const 는 타입추론을 보다 명확히 하기 위해
+const DECREASE = "counter/DECREASE" as const;
+const INCREASE_BY = "counter/INCERASE_BY" as const;
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+export const increaseBy = (diff: number) => ({
+  type: INCREASE_BY,
+  payload: diff,
+});
+
+type CounterState = {
+  count: number;
+};
+
+const initialState: CounterState = {
+  count: 0,
+};
+
+// 액션타입
+type CounterAction =
+  | ReturnType<typeof increase>
+  | ReturnType<typeof decrease>
+  | ReturnType<typeof increaseBy>;
+
+function counter(
+  state: CounterState = initialState,
+  action: CounterAction
+): CounterState {
+  switch (action.type) {
+    case INCREASE:
+      return { count: state.count + 1 };
+    case DECREASE:
+      return { count: state.count - 1 };
+    case INCREASE_BY:
+      return { count: state.count + action.payload };
+    default:
+      return state;
+  }
+}
+
+export default counter;
---
+++ src/modules/inex.ts
@@ -0,0 +1,7 @@
+import counter from "./counter";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({ counter });
+
+export default rootReducer;
+export type RootState = ReturnType<typeof rootReducer>;
